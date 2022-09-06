4efa96e8f4f5c5de9cb2ba22c37c8db1604a0ab6
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-06 16:41:37 +0500

    Update Example 003 Sum and Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 14;
-double NumberB = 5;
-double result = numberA + NumberB;
+﻿double numberA = new Random().Next(1,10); // 1 2 3 4 5 6 ... 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+double result = numberA + numberB;
 Console.WriteLine(result);
